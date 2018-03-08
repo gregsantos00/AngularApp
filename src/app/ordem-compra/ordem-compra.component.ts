0eb173464ab882b4c46df1ca576e6f2273721d80
Greg Jose dos Santos <g.santos@dom_qp.com.br>
2018-03-08 19:01:22 -0300

    Finalização de formulario com reactive forms

--- src/app/ordem-compra/ordem-compra.component.ts
+++ src/app/ordem-compra/ordem-compra.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { PedidoService } from '../pedidos.service';
 import { Pedido } from '../shared/pedido.model';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 
 @Component({
@@ -11,7 +12,12 @@ import { Pedido } from '../shared/pedido.model';
 })
 export class OrdemCompraComponent implements OnInit {
 
-  //@ViewChild('formulario') public formulario: NgForm
+  public formulario: FormGroup = new FormGroup({
+    'endereco': new FormControl(null, [Validators.required, Validators.minLength(3), Validators.maxLength(12)]),
+    'numero': new FormControl(null, [Validators.required, Validators.minLength(1), Validators.maxLength(12)]),
+    'complemento': new FormControl(null),
+    'formaPagamento': new FormControl("", [Validators.required]),
+  })
 
   public pedido: Pedido
 
@@ -19,16 +25,23 @@ export class OrdemCompraComponent implements OnInit {
 
   ngOnInit() {
   }
-  // confirmarCompra(): void {
-  //   this.pedido = new Pedido(
-  //     this.formulario.value.endereco,
-  //     this.formulario.value.numero,
-  //     this.formulario.value.complemento,
-  //     this.formulario.value.formaPagamento
-  //   )
-  //   this.service.inserirPedido(this.pedido).subscribe((x : number)=> {
-  //     this.pedido.id = x;
-  //   })
-  // }
+  confirmarCompra(): void {
+    if (!this.formulario.valid) {
+      this.formulario.get('endereco').markAsTouched();
+      this.formulario.get('numero').markAsTouched();
+      this.formulario.get('complemento').markAsTouched();
+      this.formulario.get('formaPagamento').markAsTouched();
+      return
+    }
+    this.pedido = new Pedido(
+      this.formulario.value.endereco,
+      this.formulario.value.numero,
+      this.formulario.value.complemento,
+      this.formulario.value.formaPagamento
+    )
+    this.service.inserirPedido(this.pedido).subscribe((x: number) => {
+      this.pedido.id = x;
+    })
+  }
 
 }
